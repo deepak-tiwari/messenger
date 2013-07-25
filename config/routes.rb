f2b99b0fa8de2863621aafdb5f79c6f52e98cad3
deepak-tiwari <deepak.tiwari@chronus.com>
2013-07-25 12:03:27 +0530

    relationaship added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 App1::Application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root  'static_pages#home'
   match '/home',    to: 'static_pages#home',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
